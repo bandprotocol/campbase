d22ca704c5f80ce0ad76d28bbf2040dc12c6ff59
Paul C <891585+smiled0g@users.noreply.github.com>
2018-06-04 05:58:14 +0700

    Add events

--- src/screens/Community/Events.js
+++ src/screens/Community/Events.js
@@ -1,6 +1,50 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
+import EventList from '~/components/EventList'
+
+const MonthList = [
+  {
+    monthName: 'May 2018',
+    events: [
+      {
+        date: 1,
+        name: 'Concert 15 Year Bodyslam',
+        details: '5:45 PM @Bangkok, Thailand',
+      },
+      {
+        date: 7,
+        name: 'Phuket Meet & Greet',
+        details: '12:45 PM @Phuket, Thailand',
+      },
+      {
+        date: 18,
+        name: 'Bodyslam 20 years: Live in Bangkok',
+        details: '10:20 AM @Bangkok, Thailand',
+      },
+    ],
+  },
+  {
+    monthName: 'June 2018',
+    events: [
+      {
+        date: 4,
+        name: 'Concert 15 Year Bodyslam',
+        details: '5:45 PM @Bangkok, Thailand',
+      },
+      {
+        date: 13,
+        name: 'Phuket Meet & Greet',
+        details: '12:45 PM @Phuket, Thailand',
+      },
+      {
+        date: 28,
+        name: 'Bodyslam 20 years: Live in Bangkok',
+        details: '10:20 AM @Bangkok, Thailand',
+      },
+    ],
+  },
+]
 
 export default class CommunityEventsScreen extends React.Component {
   static navigationOptions = {
@@ -11,6 +55,17 @@ export default class CommunityEventsScreen extends React.Component {
   }
 
   render() {
-    return <Text>Community Events</Text>
+    return (
+      <View>
+        {MonthList.map(month => (
+          <EventList
+            key={month.monthName}
+            title={month.monthName}
+            list={month.events}
+            onItemClick={() => false} //id => navigation.replace('CommunityTab')
+          />
+        ))}
+      </View>
+    )
   }
 }
