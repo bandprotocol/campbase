507031ade2379b642210dae9699548812fed2fc4
Paul C <pchonpimai@gmail.com>
2018-06-15 16:40:36 +0700

    Add comments in endpoint-resposne

--- server/src/common/endpoint-responses.ts
+++ server/src/common/endpoint-responses.ts
@@ -1,5 +1,13 @@
 import * as Koa from 'koa'
 
+/**
+ * Populate context with success status and
+ * standardized data format
+ *
+ * @param ctx Koa Context
+ * @param status HTTP Success status
+ * @param data Object being returned
+ */
 export function sendSuccess(
   ctx: Koa.Context,
   status: number = 200,
@@ -9,6 +17,24 @@ export function sendSuccess(
   ctx.body = { success: true, status, data }
 }
 
+/**
+ * Throws if not all the params are present in
+ * context.request.body
+ *
+ * @param ctx Koa Context
+ * @param params Array of params needed
+ */
+export function checkBodyIncludesAllParams(
+  ctx: Koa.Context,
+  params: Array<string>
+) {
+  if (params.some(param => ctx.request.body[param] === undefined))
+    throw new Exception(400, `Expect an object with ${params.join(', ')}`)
+}
+
+/**
+ * An exception to be thrown in route definitions
+ */
 export class Exception extends Error {
   constructor(public status: number, message: string) {
     super(message)
@@ -20,12 +46,4 @@ export class Exception extends Error {
       error: this.message,
     }
   }
-}
-
-export function checkBodyIncludesAllParams(
-  ctx: Koa.Context,
-  params: Array<string>
-) {
-  if (params.some(param => ctx.request.body[param] === undefined))
-    throw new Exception(400, `Expect an object with ${params.join(', ')}`)
 }
